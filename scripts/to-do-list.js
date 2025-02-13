2d7a8293d9c78ccfb44f493221d00c2a86c89a5f
pers0n4ljesus <167863953+pers0n4ljesus@users.noreply.github.com>
2025-02-14 03:09:45 +0530

    Add files via upload

---
+++ scripts/to-do-list.js
@@ -0,0 +1,33 @@
+let todoList = [];
+
+function addTodoList() {
+  const inputElement = document.querySelector('.js-todo-input');
+  const dueDateElement = document.querySelector('.js-date-input');
+  const name = inputElement.value;
+  const dueDate = dueDateElement.value;
+  const todoObject = {name, dueDate}
+  todoList.push(todoObject);
+  console.log(todoList);
+  renderTodoList();
+  inputElement.value = '';
+}
+
+function renderTodoList() {
+  let accumulatorTodo = '';
+  for( let i=0; i<todoList.length; i++) {
+    accumulatorTodo += `
+      <div class="js-todo-grid">
+        <p>${todoList[i].name}</p>
+        <p>${todoList[i].dueDate}</p>
+        <button class="js-delete-button" onclick=deleteTodo();>Delete</button>
+      </div>
+    `;
+  }
+  document.querySelector('.js-todo-list')
+  .innerHTML = accumulatorTodo;
+}
+
+function deleteTodo(i) {
+  todoList.pop(i, 0);
+  renderTodoList();
+}
